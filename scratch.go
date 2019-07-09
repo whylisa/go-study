83065b9ade3649c12f743e79bec4fd72a280c1ab
haoyue.wang <843655240@qq.com>
2019-07-09 17:51:35 +0800

    channel

--- scratch.go
+++ scratch.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //ch < - v
 //v := <-ch
 //func say (s string ) {
@@ -198,20 +196,68 @@ import "fmt"
 //}
 
 //ch := make(chan int )
-func sum(s []int,c chan int ) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum
-}
+//func sum(s []int,c chan int ) {
+//	sum := 0
+//	for _, v := range s {
+//		sum += v
+//	}
+//	c <- sum
+//}
+//
+//func main() {
+//	s := []int{7,34,234,432,42,4}
+//	c :=  make(chan int)
+//	go sum(s[:len(s)/2],c)
+//	go sum(s[len(s)/2:],c)
+//	x,y := <-c,<-c
+//
+//	fmt.Println(x,y,x+ y)
+//}
+//type Retriever struct {
+//	UserAgent string
+//	TimeOut time.Duration
+//}
+//
+//func (r *Retriever) get(url string) string {
+//	resp,err := http.Get(url)
+//	if err != nil {
+//		panic(err)
+//	}
+//	result, err := httputil.DumpResponse(
+//		resp,true)
+//	resp.Body.Close()
+//	if err != nil {
+//		panic(err)
+//	}
+//	return string (result)
+//}
+//func gerneration() chan int {
+//	out := make(chan int)
+//	go func() {
+//		i := 0
+//		for {
+//			time.Sleep(
+//				time.Duration(rand.Intn(1000)* time.Millisecond))
+//		}
+//	}
+//}
 
-func main() {
-	s := []int{7,34,234,432,42,4}
-	c :=  make(chan int)
-	go sum(s[:len(s)/2],c)
-	go sum(s[len(s)/2:],c)
-	x,y := <-c,<-c
 
-	fmt.Println(x,y,x+ y)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
