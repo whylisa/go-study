28a33edce33540850b8177ae206f19b86a0f74ec
haoyue.wang <843655240@qq.com>
2019-07-15 17:32:44 +0800

    匿名函数

--- scratch.go
+++ scratch.go
@@ -322,8 +322,25 @@ package main
 //	}
 //}
 //
-
-
+//func StringProcess(list []string, chain []func(string) string) {
+//	for index,str := range list {
+//		result := str
+//		for _,proc := range chain {
+//			result = proc(result)
+//		}
+//		list[index] = result
+//	}
+//}
+//func removePrefix(str string) string {
+//	return string.TrimPrefix(str,"go")
+//}
+//func (data int) {
+//	fmt.Println("hello",data)
+//}(100)
+//f := func(data int ) {
+//	fmt.Println("hello", data)
+//}
+//f(100)
 
 
 
