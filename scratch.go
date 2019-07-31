35bc56d6d68fded90999dbf9b2cb5405a82f5d21
haoyue.wang <843655240@qq.com>
2019-07-31 17:32:32 +0800

    竞争

--- scratch.go
+++ scratch.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
 //ch < - v
 //v := <-ch
 //func say (s string ) {
@@ -729,7 +735,27 @@ package main
 //	}
 //}
 
+var (
+	counter int
+	wg sync.WaitGroup
+)
+func main() {
+	wg.Add(2)
+	go incCounter(1)
+	go incCounter(2)
 
+	wg.Wait()
+	fmt.Println("final Counter",counter)
+}
+func incCounter(id int) {
+	defer wg.Done()
+	for count := 0;count < 2;count++ {
+		value := counter
+		runtime.Gosched()
+		value++
+		counter = value
+	}
+}
 
 
 
