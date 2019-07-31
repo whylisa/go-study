68628e0d8d8eab9fe1400797dbd3656f1888d2bf
haoyue.wang <843655240@qq.com>
2019-07-31 17:25:52 +0800

    锁

--- scratch.go
+++ scratch.go
@@ -690,10 +690,44 @@ package main
 //	fmt.Scanln(&input)
 //}
 
+//func main() {
+//	go func() {
+//		var times int
+//		for {
+//			times++
+//			fmt.Println("tick",times)
+//			time.Sleep(time.Second)
+//		}
+//	}()
+//	var input string
+//	fmt.Scanln(&input)
+//}
 
 
-
-
+//var counter int = 0
+//func Count(lock *sync.Mutex) {
+//	lock.Lock()
+//	counter++
+//	fmt.Println(z)
+//	lock.Unlock()
+//}
+//func main() {
+//	lock := &sync.Mutex{}
+//	for i :=0; i< 10;i++ {
+//		go Count(lock)
+//
+//	}
+//	for {
+//		lock.Lock()
+//		c := counter
+//
+//		lock.Unlock()
+//		runtime.Gosched()
+//		if c >=10 {
+//			break
+//		}
+//	}
+//}
 
 
 
